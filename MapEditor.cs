87c1c668c43f8817f4f4f889bfda705ccd6ddbe7
BearZerk <30960959+BearZerk@users.noreply.github.com>
2017-08-28 18:25:56 +0100

    Added Tutorial Pt1
    Added first half of the tutorial being used as an ice-breaker into Custom Editors within Unity. 
    Not fully sure of the purpose of small parts e.g. the 2 arrays instead of using 1, BUT more importantly can see what I would want to/would like to change for my own version

---
+++ MapEditor.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+[CustomEditor(typeof(Map))]
+
+public class MapEditor : Editor {
+    //Create an array for all prefabs we'll be using as tiles
+    GameObject[] prefabs;
+    //Create a single gameobject for the active tile being used
+    GameObject selectedPrefab;
+    //Create a list of Spawned tiles
+    ////CONSIDER USING A 2D ARRAY PERHAPS FOR CLARITY? OR WOULD A LIST SUIT? WOULD THE LIST HOLD EMPTY OBJECTS AND WRAP AT THE POINT OF LINE-BREAK?
+    List<GameObject> spawnedGO = new List<GameObject>();
+
+    Texture prefabTexture;
+
+    //As this is custom inspector behaviour need to update the GUI within Unity
+    public override void OnInspectorGUI()
+    {
+        //Initially draw the normal inspector view
+        DrawDefaultInspector();
+        //Create an array that holds all the tiles in the Resources > Prefabs folder. 
+        ////CAN BE CHANGED IN MY OWN VERSION TO PULL ITEMS FROM A DIFFERENT SECTION, WOULD STILL NEED TO BE RESOURCES > [FOLDERNAME]
+        Object[] obj = Resources.LoadAll("Prefabs", typeof(GameObject));
+        //Set Prefab array to the length of the number of tiles loaded in 
+        prefabs = new GameObject[obj.Length];
+        //For each object we find, set the prefab array value to the GameObject loaded into the obj array from the Prefab Folder
+        for(int i =0; i < obj.Length; i++)
+        {
+            prefabs[i] = (GameObject)obj[i];
+        }
+
+        //Create buttons in order to be able to select an active tile to be used
+        GUILayout.BeginHorizontal(); 
+        //If our array of prefabs isn't empty
+        if(prefabs != null)
+        {
+            for (int i = 0; i < prefabs.Length; i++)
+            {
+                /////THIS WORKS, ALTHOUGH IT'S HIGHLY DEPENDANT UPON HOW THE SPRITE WAS IMPORTED - SINGLE (FINE), MULTIPLE (VIEW OF ENTIRE INDEX/ATLAS OF SPRITES)
+                prefabTexture = prefabs[i].GetComponent<SpriteRenderer>().sprite.texture;
+                if(GUILayout.Button(prefabTexture, GUILayout.MaxWidth(50), GUILayout.MaxHeight(50)))
+                {
+                    selectedPrefab = prefabs[i];
+                    EditorWindow.FocusWindowIfItsOpen(typeof(Editor));
+                }
+            }   
+        }
+        GUILayout.EndHorizontal();
+    }
+}
